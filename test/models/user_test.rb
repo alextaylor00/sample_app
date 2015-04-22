bf2c5f95e5ca4466c0cf53c2aece6787f26af548
Alex Taylor <ataylor@skylabhq.com>
2015-04-22 00:37:50 +0000

    User model and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		# set up a fake valid user to use in all tests
+		@user = User.new(name: "Alex Taylor", email: "ataylor@skylabhq.com")
+	end
+
+	test "should be valid" do
+		assert @user.valid?
+	end
+
+	test "name should be present" do
+		@user.name = " "
+		assert_not @user.valid?
+	end
+
+	test "name should be 50 characters or less" do
+		@user.name = "a" * 51
+		assert_not @user.valid?
+	end
+
+
+	test "email should be present" do
+		@user.email = " "
+		assert_not @user.valid?
+	end
+
+	test "email should be 255 characters or less" do
+		@user.email = "a" * 244 + "@example.com"
+		assert_not @user.valid?
+	end
+
+	test "email should be a valid format" do
+		emails = %w[alex@example.com alex-taylor@example.com alex-taylor@example.co.uk]
+
+		emails.each do |e|
+			@user.email = e
+			assert @user.valid?, "#{e.inspect} should be valid"
+		end
+	end
+
+	test "email validation should reject invalid addresses" do
+		emails = %w[alexexample.com alex@examplecom alex@example,com]
+
+		emails.each do |e|
+			@user.email = e
+			assert_not @user.valid?, "#{e.inspect} should not be valid"
+		end		
+	end
+
+
+
+
 end
