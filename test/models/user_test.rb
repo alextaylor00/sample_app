039422162e224772a7f7b7f2d9126ce168144724
Alex Taylor <ataylor@skylabhq.com>
2015-04-23 22:49:27 +0000

    Updates user model, tests, fixtures for password addition

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,6 +51,16 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 
+	test "email address should be unique" do
+		duplicate_user = @user.dup
+		duplicate_user.email = @user.email.upcase
+		@user.save
+		assert_not duplicate_user.valid?
+	end
+
+
+
+
 
 
 end
