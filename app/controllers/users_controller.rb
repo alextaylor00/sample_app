5664d65f106f63169f67ff1424fa4a950b3a0b34
Alex Taylor <ataylor@skylabhq.com>
2015-04-26 18:47:42 +0000

    tutorial update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  def show
+  	@this_user = User.find(params[:id])
+  	#debugger
+  end
+
   def new
+  	@user = User.new # create a new user for the form to use
   end
 end
