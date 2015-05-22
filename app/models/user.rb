96f9960426f3270f04f1f59a465f26863cb49289
Alex Taylor <ataylor@skylabhq.com>
2015-05-22 22:55:39 +0000

    Finishes log in/out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token # virtual attribute, will not be stored in db
+
 	before_save do
-		email.downcase!
+		self.email = email.downcase
 	end
 
 	validates :name, presence: true, length: { maximum: 50 }
@@ -13,5 +15,34 @@ class User < ActiveRecord::Base
 
 	has_secure_password # requires bcrypt
 
+	# Remembers a user by generating a token and storing its digest in the db.
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
 
+	# Forgets a user by setting the token digest to nil.
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
+
+	# Returns true if the given token matches the digest.
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+
+	# Returns a hash digest (for password hashing)
+	# The "User." makes it a class method, not an instance method
+	def User.digest(string)
+	    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	                                                  BCrypt::Engine.cost
+	    BCrypt::Password.create(string, cost: cost)		
+	end
+
+	# Returns a random token (for remember-me)
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,34 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
-  # Add more helper methods to be used by all tests here...
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+  	!session[:user_id].nil?
+  end
+
+  # Logs in a test user.
+  def log_in_as(user, options = {})
+  	password	= options[:password]	|| 'password'
+  	remember_me = options[:remember_me]	|| '1'
+
+  	if integration_test?
+  		post login_path, session: { email: 		 user.email,
+  									password:    password,
+  									remember_me: remember_me }
+  	else
+  		session[:user_id] = user.id
+  	end
+  end
+
+  private
+
+  	# Returns true inside an integration test.
+  	def integration_test?
+  		# Ruby's defined? method returns true
+  		# if the method exists in the current context,
+  		# and post_via_redirect is only available
+  		# inside integration tests.
+  		defined?(post_via_redirect)
+  	end
+
 end
