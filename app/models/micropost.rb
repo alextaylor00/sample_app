7594783bb89a8196ee9192a876d0808f3014173c
Alex Taylor <ataylor@skylabhq.com>
2015-06-08 19:24:07 +0000

    Finishes chapter 11

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,15 @@ class Micropost < ActiveRecord::Base
   
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
 
+  mount_uploader :picture, PictureUploader
+
+  def picture_size
+  	if picture.size > 5.megabytes
+  		errors.add(:picture, "should be less than 5MB")
+  	end
+  end
 
   def self.default_scope
   	order(created_at: :desc)
